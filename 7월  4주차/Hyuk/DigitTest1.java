57b52d5349dfc1946a7663dd5ef71b4442dc6d9d
LimHyuk <skdltm117@edu.ssafy.com>
2021-07-19 22:02:50 +0900

    ^^

--- 7월  4주차/Hyuk/DigitTest1.java
+++ 7월  4주차/Hyuk/DigitTest1.java
@@ -5,7 +5,7 @@ public class DigitTest1 {
 		int num=0;
 		for(int i=0; i<5; i++) {
 			for(int j=0; j<i; j++) {
-				System.out.print("\t");
+				System.out.print("\t^^");
 			}
 			for(int k=0; k<5-i;k++) {
 				System.out.print( ++num + "\t");
