b959d95f2aa414a125a086d4cc968b70310d8351
LimHyuk <skdltm117@gmail.com>
2021-07-25 00:35:19 +0900

    Hyuk_BOJ-10989

---
+++ 7월  4주차/Hyuk/BJ_10989.java
@@ -0,0 +1,29 @@
+package BJ_Practice;
+
+import java.io.*;
+
+public class BJ_10989 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		try {
+			int N = Integer.parseInt(br.readLine());
+
+			int[] cnt = new int[10000];
+
+			for (int i = 0; i < N; i++)
+				cnt[Integer.parseInt(br.readLine())-1]++;
+
+			for (int i = 0; i < 10000; i++) {
+				for (int j = 0; j < cnt[i]; j++) {
+					bw.write(Integer.valueOf(i+1) + "\n");
+				}
+				bw.flush();
+
+			}
+			bw.close();
+			br.close();
+		} catch (Exception e) {
+		}
+	}
+}
