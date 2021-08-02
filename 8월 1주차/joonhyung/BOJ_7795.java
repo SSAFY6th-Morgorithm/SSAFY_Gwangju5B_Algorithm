ea942a162485a67209f2becae29bcff44200cd6c
KimJoonHyung <2456542@naver.com>
2021-08-02 20:22:35 +0900

    주석

--- 8월 1주차/joonhyung/BOJ_7795.java
+++ 8월 1주차/joonhyung/BOJ_7795.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 
 public class BOJ_7795 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {                     
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for(int t=0;t<T;t++) {
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			int[] nA = new int[A];
-			int[] nB = new int[B];
+		int T = sc.nextInt(); //테스트 케이스 갯수 입력
+		for(int t=0;t<T;t++) {  //테스트 케이스만큼 반복
+			int A = sc.nextInt(); //A 갯수
+			int B = sc.nextInt(); //B 갯수
+			int[] nA = new int[A]; //A 크기들
+			int[] nB = new int[B]; //B 크기들
 			for(int i=0;i<A;i++) {
 				nA[i] = sc.nextInt();
 			}
 			for(int i=0;i<B;i++) {
 				nB[i] = sc.nextInt();
 			}
-			Arrays.sort(nA);
-			Arrays.sort(nB);
+			Arrays.sort(nA); 
+			Arrays.sort(nB); //오름차순 정렬
 			
 			int result = 0;
 			for(int i=0;i<nA.length;i++)
@@ -29,7 +29,7 @@ public class BOJ_7795 {
 		}
 	}
 	
-	public static int binarySearch(int a, int[] b) {
+	public static int binarySearch(int a, int[] b) { //A원소 하나와 비교할 B크기들
 		int start = 0;
 		int end = b.length - 1;
 		int mid;
@@ -37,12 +37,12 @@ public class BOJ_7795 {
 		while(start <= end) {
 			mid = (start+end)/2;
 			if(a>b[mid]) {
-				start = mid + 1;
+				start = mid + 1; //A가 B중앙값보다 클 시 오른쪽 이동
 			}
 			else {
-				end = mid - 1;
+				end = mid - 1; //아니면 반대(크기 같아도 못먹음)
 			}
 		}
-		return start;
+		return start; //0부터 시작이라 먹을 수 있는 갯수 = start값
 	}
 }
