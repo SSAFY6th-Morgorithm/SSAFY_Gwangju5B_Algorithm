3c309e4a8e7a3d1a3e333e14cbe5b00f0e2cb9fa
yunjiyeong <mer.toonine@gmail.com>
2021-09-07 19:31:28 +0900

    ..

--- 9월 1주차/jiyeong/BJ_S2_2644_촌수계산.java
+++ 9월 1주차/jiyeong/BJ_S2_2644_촌수계산.java
@@ -42,7 +42,7 @@ public class BJ_S2_2644_촌수계산 {
 		answer = 0;
 		while(!q.isEmpty()) {
 			int qsize = q.size();
-			for (int i = 0; i < q.size(); i++) {
+			for (int i = 0; i < qsize; i++) {
 				int cur = q.poll();
 				if(cur == p2) return;
 				for (int j = 1; j <= N; j++) {
